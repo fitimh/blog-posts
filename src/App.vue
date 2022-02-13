cdcf7320057faf851360703967e3fbf1d97db98f
Fitim Hoti <fitim.hoti100@gmail.com>
2022-02-13 01:43:28 +0100

    create header and blog design

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+   <div id="app">
+      <navbar />
+      <HelloWorld />
+   </div>
+</template>
+
+<script>
+import navbar from "./components/Blogs/navbar.vue";
+import HelloWorld from "./components/Blogs/blog.vue";
+
+export default {
+   name: "App",
+   components: {
+      HelloWorld,
+      navbar,
+   },
+};
+</script>
+
+<style>
+/* #app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+} */
+</style>
