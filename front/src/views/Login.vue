e9a95cc3fc413265d7016564a4fbffcca434c4db
Fitin Hoti <fitim.hoti100@gmail.com>
2022-02-26 22:55:13 +0100

    dashboard and add folder for admin

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,19 +1,18 @@
 <template>
-   <div class="login">
-      {{ $route.params.name }}
-      <Login />
-   </div>
+  <div class="login">
+    {{ $route.params.name }}
+    <Login />
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
 import Login from "@/components/Login/Login.vue";
 
 export default {
-   name: "login",
-   components: {
-      Login,
-   },
+  name: "login",
+  components: {
+    Login,
+  },
 };
 </script>
---
+++ front/src/views/admin/Dashboard/dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="dashboard">
+    <adminDashboard/>   
+   </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import adminDashboard from "@/components/Admin/Dashboard/dashboard.vue";
+
+export default {
+   name: "dashboard",
+   components: {
+      adminDashboard,
+   },
+};
+</script>
